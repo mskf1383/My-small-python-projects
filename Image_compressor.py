1485a90cf24a966d3bcaa5189c0a8323cca2b4b3
MSKF <50908329+mskf1383@users.noreply.github.com>
2020-11-11 10:38:02 +0330

    Update Image_compressor.py

--- Image_compressor.py
+++ Image_compressor.py
@@ -15,28 +15,33 @@ def clean_screen():
 	os.system('cls' if os.name == 'nt' else 'clear')
 
 
+# The app
 def app():
 	clean_screen()
 	mskf()
 
 	try:
+		# Get the image
 		file_old = input('Enter the image adress: ')
 		file_new = file_old.replace('.', '-new.')
 		img = Image.open(file_old)
 
 		try:
+			# Save the compressed image
 			img.save(file_new, quality = 50, optimize = True)
 			input('Your image compressed successfully :)')
 
 		except:
+			# If unable to save compressed image
 			input('Failed to compress :(')
 
-		finally:
-			app()
-
 	except:
+		# If unable to import image
 		input('Failed to import image :(')
-		app()
+
+	finally:
+		app() # Run the app again
 
 
+# Run the app
 app()
