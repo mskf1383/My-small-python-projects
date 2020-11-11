f59ddb1b238c181a401d2185307ee32b59388b8a
MSKF <50908329+mskf1383@users.noreply.github.com>
2020-11-11 10:06:23 +0330

    Update Image_compressor.py

--- Image_compressor.py
+++ Image_compressor.py
@@ -5,38 +5,38 @@ import os
 def mskf():
 	print('                       _')
 	print('  /\\    /\\    /  | /  |_')
-	print(' /  \\  /  \\   \\  ||   | ')
-	print('/    \\/    \\  /  | \\  | ')
+	print(' /  \\  /  \\   \\  ||   |')
+	print('/    \\/    \\  /  | \\  |')
 	print('\n@mskf1383 - A programmer\nfrom the Depths of Mars!')
 	print('========================\n')
 
 
 def clean_screen():
 	os.system('cls' if os.name == 'nt' else 'clear')
-	
+
 
 def app():
 	clean_screen()
 	mskf()
-	
+
 	try:
 		file_old = input('Enter the image adress: ')
 		file_new = file_old.replace('.', '-new.')
 		img = Image.open(file_old)
-		
+
 		try:
 			img.save(file_new, quality = 50, optimize = True)
-			
 			input('Your image compressed successfully :)')
-			app()
 
 		except:
 			input('Failed to compress :(')
+
+		finally:
 			app()
-		
+
 	except:
 		input('Failed to import image :(')
 		app()
-		
+
 
 app()
